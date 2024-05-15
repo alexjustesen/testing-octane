a82ea9d4534bfc8c79b5fda79f139ea7b70d5fd3
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-05-15 19:09:38 -0400

    list and get users

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Models\User;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Console\Output\BufferedOutput;
@@ -18,4 +18,26 @@ Route::get('/about', function () {
     $response = json_decode($output->fetch(), true);
 
     return response()->json($response);
+});
+
+Route::get('/users', function () {
+    $users = User::query()
+        ->select(['id', 'name', 'email', 'created_at', 'updated_at'])
+        ->paginate();
+
+    return $users;
+});
+
+Route::get('/users/{user}', function (int $id) {
+    $user = User::query()
+        ->select(['id', 'name', 'email', 'created_at', 'updated_at'])
+        ->firstWhere('id', $id);
+
+    if (is_null($user)) {
+        return response()->json([
+            'message' => 'User not found.',
+        ], 404);
+    }
+
+    return $user;
 });
